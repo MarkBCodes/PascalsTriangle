fe08f6a1ed9077774f9b358ed2e6101ebdf3bf6b
Gudbrandr42 <mark.bullard42@gmail.com>
2020-03-29 12:39:12 -0400

    display Pascal's Triangle

--- src/PascalsTriangle/Program.cs
+++ src/PascalsTriangle/Program.cs
@@ -4,9 +4,27 @@ namespace PascalsTriangle
 {
     class Program
     {
-        static void Main(string[] args)
+        class Triforce
         {
-            Console.WriteLine("Hello World!");
+            public static void Main()
+            {
+                int rows = 5, val = 1, b, i, j;
+                Console.WriteLine("Pascal's triangle");
+                for (i = 0; i < rows; i++)
+                {
+                    for (b = 1; b <= rows - i; b++)
+                        Console.Write("  ");
+                    for (j = 0; j <= i; j++)
+                    {
+                        if (j == 0 || i == 0)
+                            val = 1;
+                        else
+                            val = val * (i - j + 1) / j;
+                        Console.Write(val + "   ");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
