c093a069fc85a178585abfb3439fb005c2baf7f6
Gudbrandr42 <mark.bullard42@gmail.com>
2020-03-29 12:02:48 -0400

    initial commit

---
+++ src/PascalsTriangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PascalsTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
